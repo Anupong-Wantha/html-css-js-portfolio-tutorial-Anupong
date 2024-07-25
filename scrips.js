f914a54a67048af62db97e4f9497344bd1a63e50
Anupong-Wantha <jp0951269346@gmail.com>
2024-07-25 23:36:49 +0700

    test

--- scrips.js
+++ scrips.js
@@ -5,10 +5,3 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-function toggleMenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-
-}
